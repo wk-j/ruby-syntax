20516c87f4602709e85713d605eaeec352b42d47
wk-j <somnuk.wk@gmail.com>
2018-06-12 23:51:04 +0700

    Initial commit

---
+++ CMD.md
@@ -0,0 +1,9 @@
+## Commands
+
+```
+gem install robocop
+gem install ruby-debug-ide
+gem install debase
+
+gem install solargraph
+```
---
+++ src/LearnX.rb
@@ -0,0 +1,68 @@
+
+=
+irb
+=
+
+3.class
+3.to_s
+
+1 + 1
+8 - 1
+10 * 2
+35 / 5
+1 + 1
+2 ** 5
+5 % 3
+3 & 5
+3 | 5
+3.to_s(2)
+5.to_s(2)
+(3 | 5).to_s(2)
+3 ^ 5
+(3 ^ 5).to_s(2)
+
+2 ** 5
+
+1.+ 3
+10.* 5
+nil
+
+true
+false
+
+nil.class
+true.class
+false.class
+
+1 == 1
+2 == 1
+
+1 != 1
+2 != 1
+
+!nil
+!false
+!0
+
+1 < 10
+1 > 10
+2 <= 2
+2 >= 2
+
+1 <=> 10
+10 <=> 1
+1 <=> 0
+1 <=> 1
+
+true && false
+true || false
+!true
+
+def do_somethine() {
+    return true
+}
+
+def do_somethine_else() {
+    return true
+}
+
